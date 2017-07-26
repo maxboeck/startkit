a6e78d5a5f17527fc8e73483ec82163bfb892f77
Max Böck <hello@mxb.at>
2017-07-26 16:39:21 +0200

    rm footer sidebar, add footer links

--- templates/footer.php
+++ templates/footer.php
@@ -1,14 +1,19 @@
 <?php do_action('get_footer'); ?>
 
 <footer class="footer" role="contentinfo">
-  <div class="footer__top container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
   <div class="footer__bottom container">
+
+    <?php if (has_nav_menu('footer_links')) : ?>
+    <nav class="nav nav--footer">
+      <?php wp_nav_menu(['theme_location' => 'footer_links']); ?>
+    </nav>
+    <?php endif; ?>
+
     <p>
       &copy; <?php echo date("Y") . ' ' . get_bloginfo('name') . '.'; ?> 
       <?php _e('All rights reserved.', 'startkit');?>
     </p>
+
   </div>
 </footer>
 
